0b6c5dcde2789637a42bb8fbbc8a7ce831c85deb
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-06-09 20:31:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
-  size = array.size
-  if size==1
+  if array.size==1
     array[0]
-  elsif size == 2
+  elsif array.size == 2
     "#{array[0]} and #{array[1]}"
   else
     last_item = array.pop
